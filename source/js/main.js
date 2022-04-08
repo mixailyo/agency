46be97542b4c4d16e68153893d7c7569db50c7ee
mixailyo <67204306+mixailyo@users.noreply.github.com>
2022-04-08 16:02:46 +0300

    big update

--- source/js/main.js
+++ source/js/main.js
@@ -1,46 +1,164 @@
-import {iosVhFix} from './utils/ios-vh-fix';
-import {initModals} from './modules/modals/init-modals';
+// HAMBURGER
+let hamburger = document.querySelector('.hamburger');
+let headerMenu = document.querySelector('.header__menu');
+let headerMenuItem = document.querySelectorAll('.header__menu-item');
 
-// ---------------------------------
+function openMenu() {
+  hamburger.classList.toggle('is-active');
+  headerMenu.classList.toggle('header__menu--active');
+  document.body.classList.toggle('no-scroll');
+}
 
-window.addEventListener('DOMContentLoaded', () => {
+function closeMenu() {
+  hamburger.classList.remove('is-active');
+  headerMenu.classList.remove('header__menu--active');
+  document.body.classList.remove('no-scroll');
+}
 
-  // Utils
-  // ---------------------------------
+hamburger.addEventListener('click', () => {
+  openMenu();
+})
 
-  iosVhFix();
+headerMenuItem.forEach(item => {
+  item.addEventListener('click', () => {
+    closeMenu();
+  })
+})
 
-  // Modules
-  // ---------------------------------
-
-  // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
-  // в load следует добавить скрипты, не участвующие в работе первого экрана
-  window.addEventListener('load', () => {
-    initModals();
-  });
+// Бегущая строка
+const ticker = new Swiper('.ticker', {
+  slidesPerView: 'auto',
+  speed: 12000,
+  loop: true,
+  autoplay: {
+    delay: 0,
+  },
 });
 
-// ---------------------------------
+// MODAL
+// Get the modal
+let modal = document.querySelector('[data-modal="modal"');
 
-// ❗❗❗ обязательно установите плагины eslint, stylelint, editorconfig в редактор кода.
+// Get the button that opens the modal
+let modalBtns = document.querySelectorAll('[data-modal="btn"');
 
-// привязывайте js не на классы, а на дата атрибуты (data-validate)
+// Get the <span> element that closes the modal
+let modalCloseBtn = modal.querySelector(".close");
 
-// вместо модификаторов .block--active используем утилитарные классы
-// .is-active || .is-open || .is-invalid и прочие (обязателен нейминг в два слова)
-// .select.select--opened ❌ ---> [data-select].is-open ✔️
+// When the user clicks on the button, open the modal
+modalBtns.forEach((btn) => {
+  btn.addEventListener('click', () => {
+    modal.style.display = "block";
+  })
+})
 
-// выносим все в дата атрибуты
-// url до иконок пинов карты, настройки автопрокрутки слайдера, url к json и т.д.
+// When the user clicks on <span> (x), close the modal
+modalCloseBtn.addEventListener('click', () => {
+  modal.style.display = "none";
+})
 
-// для адаптивного JS используейтся matchMedia и addListener
-// const breakpoint = window.matchMedia(`(min-width:1024px)`);
-// const breakpointChecker = () => {
-//   if (breakpoint.matches) {
-//   } else {
-//   }
-// };
-// breakpoint.addListener(breakpointChecker);
-// breakpointChecker();
+// When the user clicks anywhere outside of the modal, close it
+document.addEventListener('click', (e) => {
+  if (e.target == modal) {
+    modal.style.display = "none";
+  }
+})
 
-// используйте .closest(el)
+// Отправка заявки из формы в телеграм
+let modalForm = document.querySelector('.modal-form');
+let modalSubmit = document.querySelector('.modal-submit');
+let modalFromTitle = document.querySelector('.modal-from__title');
+let modalInputName = document.querySelector('.modal-input--name');
+let modalInputTel = document.querySelector('.modal-input--tel');
+let botToken = '5288895801:AAEdIRj9GSDxwsVJdeTYYBlYyftbtfrSxg8';
+let chatId = '-1001675747485';
+
+modalForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  let name = modalInputName.value;
+  let tel = modalInputTel.value;
+
+  text = `Всем приветики, мои сладкие! ❤️ Прилетела заявочка от писечки по имени <b>${name}</b> с номером телефона <b>${tel}</b>`;
+  
+  fetch(`https://api.telegram.org/bot${botToken}/sendMessage?chat_id=${chatId}&parse_mode=html&text=${text}`)
+
+  modalFromTitle.textContent = 'Ваша заявка отправлена!'
+  modalFromTitle.style.marginBottom = '0';
+  modalInputName.style.display = 'none';
+  modalInputTel.style.display = 'none';
+  modalSubmit.style.display = 'none';
+
+  setTimeout(() => {
+    modal.click();
+  }, 3000)
+})
+
+// Portfolio
+document.addEventListener("DOMContentLoaded", () => {
+  let portfolioPhotosList = document.querySelector('.portfolio__photos-list');
+
+  let i = 0;
+  let arrayPhotos = [];
+
+  function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  while (arrayPhotos.length < 19) {
+    let newNumber = getRandomIntInclusive(0, 18);
+    if (arrayPhotos.indexOf(newNumber) === -1) {
+      arrayPhotos.push(newNumber)
+    }
+  }
+
+  while (i <= 18) {
+    let newPortfolioPhotosListItem = document.createElement('li');
+    newPortfolioPhotosListItem.classList.add('portfolio__photos-list-item');
+
+    let newPortfolioPhotoLink = document.createElement('a');
+    newPortfolioPhotoLink.href = `../img/portfolio-${arrayPhotos[i]}.jpg`;
+    newPortfolioPhotoLink.dataset.fancybox = 'gallery';
+
+    let newPortfolioPhoto = document.createElement('img');
+    newPortfolioPhoto.src = `../img/portfolio-${arrayPhotos[i]}-preview.jpg`;
+    newPortfolioPhoto.classList.add('portfolio__photo');
+
+    newPortfolioPhotoLink.append(newPortfolioPhoto)
+    newPortfolioPhotosListItem.append(newPortfolioPhotoLink)
+    portfolioPhotosList.append(newPortfolioPhotosListItem)
+
+    i++
+  }
+
+  let portfolioPhoto = document.querySelectorAll('.portfolio__photo');
+
+  function hidePhotos(target) {
+    portfolioPhoto.forEach((otherItem) => {
+      if (otherItem != target) {
+        otherItem.style.opacity = 0
+        otherItem.style.transition = `opacity ${0.3 * getRandomIntInclusive(1, 7)}s linear`
+      }
+    })
+  }
+
+  function showPhotos() {
+    portfolioPhoto.forEach(item => {
+      item.style.opacity = 1
+    })
+  }
+
+  let counter
+
+  document.addEventListener('mouseover', (e) => {
+    clearTimeout(counter)
+    if (e.target.classList.contains('portfolio__photo')) {
+      counter = setTimeout(() => {
+        hidePhotos(e.target)
+        e.target.addEventListener('mouseout', showPhotos)
+      }, 3000);
+    }
+  })
+})
